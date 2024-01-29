8515e9421c91407a492a07ed6bc2fa6ffbc24652
Soros87 <timkwan87@gmail.com>
2024-01-29 14:24:24 +0800

    set up Usermodel in mongodb

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+//schema
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name is Required!"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last Name is Required!"],
+    },
+    email: {
+      type: String,
+      required: [true, " Email is Required!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required!"],
+      minlength: [6, "Password length should be greater than 6 character"],
+      select: true, // Excludes 'password' by default from query results
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
