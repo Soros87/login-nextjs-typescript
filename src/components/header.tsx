9fdceddb5ed708ce40f11c92eb0ef96489e1322f
Soros87 <timkwan87@gmail.com>
2024-01-29 17:30:00 +0800

    fix home page header login and logout button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,43 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Header = () => {
+  const { status } = useSession();
   return (
     <header className="z-[999] relative flex justify-between items-center">
       <div className="flex flex-row fixed top-0 left-0 right-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]">
         <div className="fixed top-0 right-10 h-[4.5rem] flex items-center gap-5">
-          <span>Welcome</span>
-          <Image
-            src="/userprofile.png"
-            alt=""
-            width={40}
-            height={40}
-            className="object-contain"
-          />
-          <button
-            title="Log Out"
-            className="text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2 border border-[#666] rounded-full"
-          >
-            Logout
-          </button>
+          {status === "authenticated" ? (
+            <>
+              <span>Welcome</span>
+              <Image
+                src="/userprofile.png"
+                alt=""
+                width={40}
+                height={40}
+                className="object-contain"
+              />
+              <button
+                title="Log Out"
+                className="text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2 border border-[#666] rounded-full"
+                onClick={() => signOut()}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <Link href="/login">
+              <button
+                title="Login"
+                className="text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2 border border-[#666] rounded-full"
+              >
+                Login
+              </button>
+            </Link>
+          )}
         </div>
       </div>
     </header>
